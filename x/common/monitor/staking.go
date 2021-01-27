fbe6043719c3bbee99b34c5097d0e05c63efeb48
Green <gl12138@live.com>
2021-01-27 21:07:08 +0800

    add staking metrics

--- x/common/monitor/staking.go
+++ x/common/monitor/staking.go
@@ -9,9 +9,12 @@ import (
 
 // StakingMetric is the struct of metric in order module
 type StakingMetric struct {
-	AllValidatorsShare             metrics.Gauge
-	ControlledValidatorsShare      metrics.Gauge
-	ControlledValidatorsShareRatio metrics.Gauge
+	AllValidatorsShare                         metrics.Gauge
+	ControlledValidatorsShare                  metrics.Gauge
+	ControlledValidatorsShareRatio             metrics.Gauge
+	AllValidatorsAndCandidateShare             metrics.Gauge
+	ControlledValidatorsAndCandidateShare      metrics.Gauge
+	ControlledValidatorsAndCandidateShareRatio metrics.Gauge
 }
 
 // DefaultOrderMetrics returns Metrics build using Prometheus client library if Prometheus is enabled
@@ -48,14 +51,35 @@ func NewStakingMetric(labelsAndValues ...string) *StakingMetric {
 			Name:      "controlled_validators_share_ratio",
 			Help:      "the ratio of all contraolled validators share to all validators share",
 		}, labels).With(labelsAndValues...),
+		AllValidatorsAndCandidateShare: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: xNameSpace,
+			Subsystem: stakingSubSystem,
+			Name:      "all_validators_and_candidate_share",
+			Help:      "the total share of all validators and candidate",
+		}, labels).With(labelsAndValues...),
+		ControlledValidatorsAndCandidateShare: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: xNameSpace,
+			Subsystem: stakingSubSystem,
+			Name:      "controlled_validators_and_candidate_share",
+			Help:      "the total share of all contraolled validators and candidate",
+		}, labels).With(labelsAndValues...),
+		ControlledValidatorsAndCandidateShareRatio: prometheus.NewGaugeFrom(stdprometheus.GaugeOpts{
+			Namespace: xNameSpace,
+			Subsystem: stakingSubSystem,
+			Name:      "controlled_validators_and_candidate_share_ratio",
+			Help:      "the ratio of all contraolled validators share to all validators share and candidate",
+		}, labels).With(labelsAndValues...),
 	}
 }
 
 // NopStakingMetric returns a pointer of a no-op Metrics
 func NopStakingMetric() *StakingMetric {
 	return &StakingMetric{
-		AllValidatorsShare:             discard.NewGauge(),
-		ControlledValidatorsShare:      discard.NewGauge(),
-		ControlledValidatorsShareRatio: discard.NewGauge(),
+		AllValidatorsShare:                         discard.NewGauge(),
+		ControlledValidatorsShare:                  discard.NewGauge(),
+		ControlledValidatorsShareRatio:             discard.NewGauge(),
+		AllValidatorsAndCandidateShare:             discard.NewGauge(),
+		ControlledValidatorsAndCandidateShare:      discard.NewGauge(),
+		ControlledValidatorsAndCandidateShareRatio: discard.NewGauge(),
 	}
 }
